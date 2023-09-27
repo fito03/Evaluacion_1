e8843f69d8caf9a435fb4987cf315af0fc049d5b
Rodolfo Rivas <rivas.fito03@gmail.com>
2023-09-27 18:57:34 -0300

    Creacion renderProducto

--- Evaluacion_1APP/views.py
+++ Evaluacion_1APP/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def renderIndex(request):
     return render(request, 'templatesAPP1/index.html')
+
+def renderProductos(request):
+    return render(request, 'templatesAPP1/productos.html')
